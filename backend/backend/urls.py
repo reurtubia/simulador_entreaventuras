3fff849a8b629c2568d3b31ec573e0e0c6c770d1
reurtubia <reurtubia@uc.cl>
2022-10-03 09:21:25 -0300

    CHG: [backend] Cambio nombre

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Vinken API')
+schema_view = get_swagger_view(title='DnD Chile API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
